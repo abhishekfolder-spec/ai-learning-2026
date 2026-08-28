e06e40e4cb594aadcac4957d78a51353c0c9ca03
Ab Sh <abhishek.folder@gmail.com>
2026-08-28 22:30:14 +0530

    Day - Python basics

---
+++ 01_python/day01/hello_ai.py
@@ -0,0 +1,13 @@
+name = 'Ab'
+experience = 20
+goal = "Become an AI systmes builder"
+current_year = 2026
+target_day = 180
+
+
+
+print ("Name", name)
+print ("Experience:",experience,"years")
+print ("Goal",goal)
+print ("Starting Year", current_year)
+print ("Target:","Day",target_day)
---
+++ README.md
@@ -0,0 +1,11 @@
+#AI Learning 2026
+
+My 180-day journey to become an AI systems buidler 
+
+## Goals
+
+-AI engineering
+-Workflow automation
+-AI security
+-Equity Research
+-Portfolio Optimization
